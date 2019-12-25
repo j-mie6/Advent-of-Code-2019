43f053aba75ac069f6bdcdfdf911570254b0f217
Jamie Willis <jw14896@my.bristol.ac.uk>
2019-12-25 12:30:43 +0000

    Refactored to support big integers (day 8)

--- IntCode.hs
+++ IntCode.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE PatternSynonyms #-}
 module IntCode where
 import Data.Array.Base
 import Control.Monad
@@ -13,11 +14,14 @@ import Control.Monad.Cont
 import Control.Monad.Reader
 import Control.Monad.Trans
 
+type Val = Integer
+type Idx = Val
+type Op = Val
 type MonadIntCode m t = ( MonadTrans t
                         , MonadReader (() -> t m ()) (t m)
                         , MonadCont (t m)
                         , MonadInput m Int) 
-type Intcode m t arr = (MArray arr Int m, MonadIntCode m t)
+type Intcode m t arr = (MArray arr Idx m, MonadIntCode m t)
 newtype IntCode r m a = IntCode {runIntCode :: ReaderT (() -> IntCode r m ()) (ContT r m) a}
   deriving Functor
   deriving Applicative
@@ -43,7 +47,7 @@ instance (Monad m, MonadTrans t, Monad (t m), MArray arr e m)
   unsafeRead arr idx = lift (unsafeRead arr idx)
   unsafeWrite arr idx x = lift (unsafeWrite arr idx x)
   
-liftOp :: Intcode m t arr => (Int -> Int -> Int) -> arr Int Int -> Int -> Int -> Int -> t m ()
+liftOp :: Intcode m t arr => (Val -> Val -> Val) -> arr Idx Val -> Idx -> Idx -> Idx -> t m ()
 liftOp op arr dst src1 src2 =
   do x <- readArray arr src1
      y <- readArray arr src2
@@ -57,14 +61,25 @@ noargs m _ _ _ _ = m
 exit :: MonadIntCode m t => t m ()
 exit = join (asks ($ ()))
 
-decode :: Intcode m t arr => Int -> (arr Int Int -> Int -> Int -> Int -> t m ())
-decode 1  = liftOp (+)
-decode 2  = liftOp (*)
-decode 99 = noargs exit
+pattern Add  <- 1
+pattern Mult <- 2
+pattern In   <- 3
+pattern Out  <- 4
+pattern Off  <- 9
+pattern Halt <- 99
+
+pattern Pos <- 0
+pattern Imm <- 1
+pattern Rel <- 2
+
+decode :: Intcode m t arr => Op -> (arr Idx Val -> Idx -> Idx -> Idx -> t m ())
+decode Add  = liftOp (+)
+decode Mult = liftOp (*)
+decode Halt = noargs exit
 decode _  = noargs (fail "Unknown opcode")
 
 -- This will need remodelling for when the opcode has a varying length
-fetch :: Intcode m t arr => arr Int Int -> Int -> t m (Int, Int, Int, Int, Int)
+fetch :: Intcode m t arr => arr Idx Val -> Idx -> t m (Idx, Op, Val, Val, Val)
 fetch arr i =
     do opcode <- readArray arr i
        src1   <- readArray arr (i + 1)
@@ -72,12 +87,12 @@ fetch arr i =
        dest   <- readArray arr (i + 3)
        return (i + 4, opcode, src1, src2, dest)
 
-initialise :: (Monad m, MArray arr Int m) => [Int] -> m (arr Int Int)
-initialise xs = newListArray (0, m) xs
+initialise :: (Monad m, MArray arr Val m) => [Val] -> m (arr Idx Val)
+initialise xs = newListArray (0, fromIntegral m) xs
   where n = length xs
         m = n + (4 - n `mod` 4)
 
-execute :: (MonadInput m Int, MArray arr Int m) => arr Int Int -> m Int--(arr Int Int)
+execute :: (MonadInput m Int, MArray arr Val m) => arr Idx Val -> m Val--(arr Int Int)
 execute arr = flip runContT return $
   -- callCC registers the readArray arr 0 as the place where code should return to
   -- when onExit is called, beautiful beautiful technique
--- day2.hs
+++ day2.hs
@@ -10,7 +10,7 @@ instance (Read a, Show a) => MonadInput IO a where
   input  = readLn
   output = print
 
-runWithValues :: Int -> Int -> IO Int
+runWithValues :: Val -> Val -> IO Val
 runWithValues x y =
   do xs <- Main.input
      arr <- initialise @IO @IOArray xs
@@ -18,14 +18,14 @@ runWithValues x y =
      writeArray arr 2 y
      execute arr
 
-task1 :: IO Int
+task1 :: IO Val
 task1 = runWithValues 12 2
 
 -- The ListT monad, though usually dodgy, works well here
 -- It does not, however, short circuit
 -- We non-deterministically choose two numbers between 0 to 99
 -- We run this program with those values
-task2 :: IO [(Int, Int)]
+task2 :: IO [(Val, Val)]
 task2 = runListT $
   do let val = 19690720
      x <- ListT (return [0..99])
@@ -41,5 +41,5 @@ split f = foldr g [[]]
       | f x  = []:ys:yss
       | otherwise = (x:ys):yss
 
-input :: IO [Int]
+input :: IO [Val]
 input = map read . split (== ',') <$> readFile "day2-input.txt"
