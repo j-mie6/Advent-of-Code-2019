0b9fdf2b9f497fc1745737e6eb817bde506a0925
Jamie Willis <jw14896@my.bristol.ac.uk>
2019-12-25 13:42:59 +0000

    Added a state into the machine

--- IntCode.hs
+++ IntCode.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
@@ -13,20 +12,36 @@ import Data.Array.Base
 import Control.Monad
 import Control.Monad.Cont
 import Control.Monad.Reader
+import Control.Monad.State
 import Control.Monad.Trans
 
 type MonadIntCode m t x = ( MonadTrans t
                           , MonadReader (() -> t m ()) (t m)
+                          , MonadState (IntState x) (t m)
                           , MonadCont (t m)
                           , MonadInput m x)
 type Intcode m t arr x = (Ix x, Num x, MArray arr x m, MonadIntCode m t x)
-newtype IntCode r m a = IntCode {runIntCode :: ReaderT (() -> IntCode r m ()) (ContT r m) a}
+
+pattern Add  <- 1
+pattern Mult <- 2
+pattern In   <- 3
+pattern Out  <- 4
+pattern Off  <- 9
+pattern Halt <- 99
+
+pattern Pos <- 0
+pattern Imm <- 1
+pattern Rel <- 2
+
+data IntState x = IntState { pc :: x }
+newtype IntCode r x m a = IntCode {runIntCode :: StateT (IntState x) (ReaderT (() -> IntCode r x m ()) (ContT r m)) a}
   deriving Functor
   deriving Applicative
   deriving Monad
-  deriving (MonadReader (() -> IntCode r m ()))
+  deriving (MonadReader (() -> IntCode r x m ()))
+  deriving (MonadState (IntState x))
   deriving MonadCont
-instance MonadTrans (IntCode r) where lift m = IntCode (lift (lift m))
+instance MonadTrans (IntCode r x) where lift m = IntCode (lift (lift (lift m)))
 
 class Monad m => MonadInput m a where
   input  :: m a
@@ -52,38 +67,35 @@ liftOp op arr dst src1 src2 =
      writeArray arr dst (op x y)
 
 noargs m _ _ _ _ = m
+
 -- This is magical, when this is executed then
 -- it jumps to the continuation point stored by callCC
 -- this has the effect of skipping the rest of the code
-
 exit :: MonadReader (() -> m ()) m => m ()
 exit = join (asks ($ ()))
 
-pattern Add  <- 1
-pattern Mult <- 2
-pattern In   <- 3
-pattern Out  <- 4
-pattern Off  <- 9
-pattern Halt <- 99
+getPc :: MonadState (IntState x) m => m x
+getPc = gets pc
 
-pattern Pos <- 0
-pattern Imm <- 1
-pattern Rel <- 2
+putPc :: MonadState (IntState x) m => x -> m ()
+putPc pc = modify (\s -> s {pc = pc})
 
 decode :: Intcode m t arr x => x -> (arr x x -> x -> x -> x -> t m ())
 decode Add  = liftOp (+)
 decode Mult = liftOp (*)
 decode Halt = noargs exit
-decode _  = noargs (fail "Unknown opcode")
+decode _    = noargs (fail "Unknown opcode")
 
 -- This will need remodelling for when the opcode has a varying length
-fetch :: Intcode m t arr x => arr x x -> x -> t m (x, x, x, x, x)
-fetch arr i =
-    do opcode <- readArray arr i
+fetch :: Intcode m t arr x => arr x x -> t m (x, x, x, x)
+fetch arr =
+    do i <- getPc
+       opcode <- readArray arr i
        src1   <- readArray arr (i + 1)
        src2   <- readArray arr (i + 2)
        dest   <- readArray arr (i + 3)
-       return (i + 4, opcode, src1, src2, dest)
+       putPc (i + 4)
+       return (opcode, src1, src2, dest)
 
 initialise :: (MArray arr x m, Ix x, Num x, Monad m) => [x] -> m (arr x x)
 initialise xs = newListArray (0, fromIntegral m) xs
@@ -94,13 +106,13 @@ execute :: (Ix x, Num x, MonadInput m x, MArray arr x m) => arr x x -> m x--(arr
 execute arr = flip runContT return $
   -- callCC registers the readArray arr 0 as the place where code should return to
   -- when onExit is called, beautiful beautiful technique
-  do callCC (\onExit -> runReaderT (runIntCode (exec 0)) (IntCode . lift . onExit))
-     lift $ readArray arr 0
+  do callCC (\onExit -> runReaderT (evalStateT (runIntCode exec) (IntState 0)) (IntCode . lift . lift . onExit))
+     readArray arr 0
      --return arr
   where
-    exec pc = do (pc', opcode, src1, src2, dest) <- fetch arr pc
-                 decode opcode arr dest src1 src2
-                 exec pc'
+    exec = do (opcode, src1, src2, dest) <- fetch arr
+              decode opcode arr dest src1 src2
+              exec
 
 {-execIntCode :: [Int] -> IO Int
 execIntCode = initialise @IO @IOArray >=> execute
