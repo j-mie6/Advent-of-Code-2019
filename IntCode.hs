9add5ccedb70ad386c479ae451806d842431a9ee
Jamie Willis <jw14896@my.bristol.ac.uk>
2019-12-25 16:48:32 +0000

    Made the exit return the current memory, this will allow for resizing

--- IntCode.hs
+++ IntCode.hs
@@ -19,7 +19,7 @@ type MonadIntCode m t arr x = ( Ix x
                               , Num x
                               , MArray arr x m
                               , MonadTrans t
-                              , MonadReader (() -> t m ()) (t m)
+                              , MonadReader (arr x x -> t m ()) (t m)
                               , MonadState (IntState arr x) (t m)
                               , MonadCont (t m)
                               , MonadInput m x)
@@ -36,11 +36,11 @@ pattern Imm <- 1
 pattern Rel <- 2
 
 data IntState arr x = IntState { pc :: x, mem :: arr x x }
-newtype IntCode r arr x m a = IntCode {runIntCode :: StateT (IntState arr x) (ReaderT (() -> IntCode r arr x m ()) (ContT r m)) a}
+newtype IntCode r arr x m a = IntCode {runIntCode :: StateT (IntState arr x) (ReaderT (arr x x -> IntCode r arr x m ()) (ContT r m)) a}
   deriving Functor
   deriving Applicative
   deriving Monad
-  deriving (MonadReader (() -> IntCode r arr x m ()))
+  deriving (MonadReader (arr x x -> IntCode r arr x m ()))
   deriving (MonadState (IntState arr x))
   deriving MonadCont
 instance MonadTrans (IntCode r arr x) where lift m = IntCode (lift (lift (lift m)))
@@ -65,11 +65,20 @@ instance (Monad m, MonadTrans t, Monad (t m), MArray arr e m)
 -- This is magical, when this is executed then
 -- it jumps to the continuation point stored by callCC
 -- this has the effect of skipping the rest of the code
-exit :: MonadReader (() -> m ()) m => m ()
-exit = join (asks ($ ()))
+exit :: (MonadState (IntState arr x) m , MonadReader (arr x x -> m ()) m) => m ()
+exit = do arr <- gets mem
+          join (asks ($ arr))
 
-incPc :: Num x => MonadState (IntState arr x) m => x -> m ()
+incPc :: (Num x, MonadState (IntState arr x) m) => x -> m ()
 incPc i = modify (\s -> s {pc = pc s + i})
+
+readMem :: MonadIntCode m t arr x => x -> t m x
+readMem idx = do arr <- gets mem
+                 readArray arr idx
+
+writeMem :: MonadIntCode m t arr x => x -> x -> t m ()
+writeMem idx x = do arr <- gets mem
+                    writeArray arr idx x
 
 data Fetched x = Fetched 
   { op :: x 
@@ -86,38 +95,37 @@ size NoOps = 0
 fetch :: MonadIntCode m t arr x => t m (Fetched x)
 fetch =
     do i <- gets pc
-       arr <- gets mem
-       opcode <- readArray arr i
+       opcode <- readMem i
        ops <- case opcode of
-         Add  -> readBinary arr i
-         Mult -> readBinary arr i
-         In   -> readUnary arr i
-         Out  -> readUnary arr i
-         Off  -> readUnary arr i
+         Add  -> readBinary i
+         Mult -> readBinary i
+         In   -> readUnary i
+         Out  -> readUnary i
+         Off  -> readUnary i
          Halt -> return NoOps
+         _    -> fail "Unknown opcode"
        incPc (size ops + 1)
        return (Fetched opcode ops)
     where
-      readUnary arr i   = UnaryOp <$> readArray arr (i + 1)
-      readBinary arr i  = liftM3 BinOp (readArray arr (i + 1)) 
-                                       (readArray arr (i + 2)) 
-                                       (readArray arr (i + 3))
+      readUnary i   = UnaryOp <$> readMem (i + 1)
+      readBinary i  = liftM3 BinOp (readMem (i + 1)) 
+                                   (readMem (i + 2)) 
+                                   (readMem (i + 3))
 
 liftBin :: MonadIntCode m t arr x => (x -> x -> x) -> Ops x -> t m ()
-liftBin op (BinOp src1 src2 dst) =
-  do arr <- gets mem
-     x <- readArray arr src1
-     y <- readArray arr src2
-     writeArray arr dst (op x y)
+liftBin op (BinOp src1 src2 dst) = 
+  liftM2 op (readMem src1) (readMem src2) >>= writeMem dst
 
 decode :: MonadIntCode m t arr x => Fetched x -> t m ()
-decode (Fetched Add ops)         = liftBin (+) ops
-decode (Fetched Mult ops)        = liftBin (*) ops
-decode (Fetched In (UnaryOp x))  = undefined
-decode (Fetched Out (UnaryOp x)) = undefined
-decode (Fetched Off (UnaryOp x)) = undefined
-decode (Fetched Halt NoOps)      = exit
-decode _                         = fail "Unknown opcode"
+decode (Fetched Add ops)           = liftBin (+) ops
+decode (Fetched Mult ops)          = liftBin (*) ops
+decode (Fetched In (UnaryOp dst))  = do x <- input
+                                        writeMem dst x
+decode (Fetched Out (UnaryOp src)) = do x <- readMem src
+                                        output x
+--decode (Fetched Off (UnaryOp x))   = undefined
+decode (Fetched Halt NoOps)        = exit
+decode _                           = fail "Unknown opcode"
 
 initialise :: (MArray arr x m, Ix x, Num x, Monad m) => [x] -> m (arr x x)
 initialise xs = newListArray (0, fromIntegral (length xs)) xs
@@ -126,9 +134,12 @@ execute :: (Ix x, Num x, MonadInput m x, MArray arr x m) => arr x x -> m x--(arr
 execute arr = flip runContT return $
   -- callCC registers the readArray arr 0 as the place where code should return to
   -- when onExit is called, beautiful beautiful technique
-  do callCC (\onExit -> runReaderT (evalStateT (runIntCode exec) (IntState 0 arr)) (IntCode . lift . lift . onExit))
-     readArray arr 0
-     --return arr
+  do arr' <- callCC (\onExit -> 
+       flip runReaderT (IntCode . lift . lift . onExit) $
+         flip evalStateT (IntState 0 arr) $
+           runIntCode exec)
+     readArray arr' 0
+     --return arr'
   where
     exec = do fetched <- fetch
               decode fetched
