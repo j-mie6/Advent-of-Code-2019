17c7483610f439e7ab5e920425b90ebec8581462
Jamie Willis <jw14896@my.bristol.ac.uk>
2019-12-26 13:26:13 +0000

    This _should_ work, but it's in a looping state...

--- day7.hs
+++ day7.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE OverlappingInstances #-}
 import Data.Array.ST
 import Control.Monad.ST
+import Control.Monad.Cont
 import Data.List
 import IntCode
 import Pipe
@@ -14,18 +15,36 @@ instance Monad m => MonadInput (PipeT a m) a where
 mkProgram :: [Int] -> PipeT Int (ST s) (STArray s Int Int)
 mkProgram = initialise
 
-ampControl :: [Int] -> Int -> Int -> Int
-ampControl xs phase input = 
-  let (_, [output]) = runST (runPipeT (mkProgram xs >>= execute) [phase, input])
+ampControl :: [Int] -> [Int] -> [Int]
+ampControl xs is = snd (runST (runPipeT (mkProgram xs >>= execute) is))
+
+feed :: [Int] -> Int -> Int -> Int
+feed xs phase input = 
+  let [output] = ampControl xs [phase, input]
   in output
 
 pipeline :: [Int] -> [Int] -> Int
-pipeline xs = foldl (flip (ampControl xs)) 0
+pipeline xs = foldl (flip (feed xs)) 0
 
 findMax :: IO Int
 findMax = 
   do xs <- Main.input
-     return (maximum (map (pipeline xs) (permutations [0, 1, 2, 3, 4])))
+     return (maximum (map (pipeline xs) (permutations [0..4])))
+
+-- The 5 amplifiers need to be connected in a circle and
+-- given 5-9 as their phases, the first is fed 0 and they
+-- will cycle round until the final one outputs one more
+-- value, so we are forcing the `last` of the final os
+loop :: [Int] -> [Int] -> [Int] -> [Int]
+loop xs = foldr (\p next is1 -> ampControl xs (p : next is1)) id
+
+tie :: [Int] -> [Int] -> Int
+tie xs ps = let os = loop xs ps (0 : init os) in last os
+
+findMaxLoop :: IO Int
+findMaxLoop =
+  do xs <- Main.input
+     return (maximum (map (tie xs) (permutations [5..9])))
 
 split :: (a -> Bool) -> [a] -> [[a]]
 split f = foldr g [[]]
